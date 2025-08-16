86761fc7d3420da165409c16ea5fb177c87ecaf1
Yousab Fouad <yosab.fouad@gmail.com>
2025-08-16 19:15:57 +0300

    making some upgrades to remove the done tasks

--- todo-list.js
+++ todo-list.js
@@ -1,10 +1,6 @@
-const todoListObject = [
-  {
-    name: 'Your To DO',
-    dueDate: 'Due Date',
-  },
-];
-todoListObject.splice(0, 1);
+const todoListObject = [];
+const doneTasks = [];
+
 function addTodo() {
   const inputElement = document.querySelector('.todo-space');
   const name = inputElement.value;
@@ -14,11 +10,13 @@ function addTodo() {
     alert('Please enter a todo name');
     return; // Exit the function if empty
   }
+
   const dateElement = document.querySelector('.js-due-date');
   let dueDate = dateElement.value;
   if (dueDate === '') {
     dueDate = new Date().toLocaleDateString('en-GB', { timeZone: 'UTC' });
   }
+
   todoListObject.push({ name, dueDate });
   inputElement.value = '';
   dateElement.value = '';
@@ -30,24 +28,52 @@ function renderTodoList() {
   for (let i = 0; i < todoListObject.length; i++) {
     const todos = todoListObject[i];
     const { name, dueDate } = todos;
-    const html = `<div>
-    <input type="checkbox"  id="name"/ >
-    <label for="name"> ${name}
-    </label>
-    </div>
-   <div> ${dueDate}</div>
-    <button onclick=" 
-    todoListObject.splice(${i},1);
-    renderTodoList();
-  " class="delete-todo-button">Delete</button>
+    const checkboxId = `todo-${i}`;
+
+    const html = `
+      <div>
+        <input type="checkbox" id="${checkboxId}" onchange="taskChecked(${i})" />
+        <label for="${checkboxId}" id="label-${i}">${name}</label>
+      </div>
+      <div>${dueDate}</div>
+      <button onclick=" 
+        todoListObject.splice(${i},1);
+        renderTodoList();
+      " class="delete-todo-button">Delete</button>
     `;
     todoListHTML += html;
   }
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
 
+function renderDoneList() {
+  let doneListHTML = '<h3>Tasks I Have Done</h3> <hr> <br>';
+  for (let i = 0; i < doneTasks.length; i++) {
+    const { name, dueDate } = doneTasks[i];
+    doneListHTML += `
+    
+      <div>
+        <label style="text-decoration: line-through;">${name}</label>
+      </div>
+      <div>${dueDate}</div>
+      <br>
+    `;
+  }
+  document.querySelector('.js-done-list').innerHTML = doneListHTML;
+}
+
 function checkKey(event) {
   if (event.key === 'Enter') {
     addTodo();
+  }
+}
+
+function taskChecked(index) {
+  const checkbox = document.getElementById(`todo-${index}`);
+  if (checkbox.checked) {
+    const task = todoListObject.splice(index, 1)[0];
+    doneTasks.push(task);
+    renderTodoList();
+    renderDoneList();
   }
 }
